9159063bc777258eb11b29c0dbd3f725a4a367f8
mateusz97i1 <120130259+mateusz97i1@users.noreply.github.com>
2026-04-16 20:32:04 +0200

    working on saving fiiles to pdf

--- vin_decoder/views.py
+++ vin_decoder/views.py
@@ -4,17 +4,19 @@ import os
 import requests
 import logging
 import markdown2
+import io
 
 from .forms import InputVinForm
 from django.shortcuts import render , redirect
 from .services import get_vehicle_data_vin, openai_prompt_basic
-
+from xhtml2pdf import pisa
+from django.http import FileResponse
 
 #create logger
 logger = logging.getLogger(__name__)
 
 #focus rs vin number
-test_vin = "WF0DP3TH6H4123982"
+test_vin = "WBA5U9C00LFJ37061"
 
 
 @require_GET
@@ -42,6 +44,33 @@ def home(request):
         })
 
 
+def save_reasults_pdf(html_source,car_description):
+    """Take generated raport in memory and save it as pdf using xhtml2pdf"""
+
+    pisa.showLogging()
+
+    output = io.BytesIO()
+
+#  create pdf in memory
+    pisa_status = pisa.CreatePDF(
+        html_source,
+        dest= output
+    )
+
+    if pisa_status.err:
+        logger.error(f"Error saving PDF: {pisa_status.err}")
+
+    # set cursor in memory at the beggining
+    output.seek(0)
+
+    # return file with according name, file extension (forces download in web browser) 
+    return FileResponse(
+        output,
+        as_attachment= True,
+        filename= f"Raport+{car_description}.pdf"
+    )
+
+
 @require_POST
 def openai_common_car_issues(request):
     """
@@ -58,7 +87,8 @@ def openai_common_car_issues(request):
     task_map = {
         "common_issues": "tell me about 3 typical issues with this car model",
         "price_range": "tell me the average price range for this car model in the current market",
-        "millage_range": "tell me the average mileage range at which major services are usually needed for this model"
+        "millage_range": "tell me the average mileage range at which major services are usually needed for this model",
+        "save_pdf": "provide detailed information about this car model"
     }
 
     # get selected task for button
@@ -78,11 +108,21 @@ def openai_common_car_issues(request):
 
 
     results_html, message_error = openai_prompt_basic(car_description, system_prompt)
+
+    if action == "save_pdf":
+
+       pdf_response =  save_reasults_pdf(results_html,car_description)
+
+       if pdf_response:
+           
+           return pdf_response
+
             
     # Return onlyu partial from templates/partials/
     return render(request, 'partials/gpt_typical_issues_car.html', 
                     context= {
                     'results_html': results_html,
-                    'message_error': message_error
+                    'message_error': message_error,
+                    'car_description': car_description
                     })
             
