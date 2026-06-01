6bd5998d20cc2bcabdf520455f4ae5a714ce96b6
mateusz97i1 <120130259+mateusz97i1@users.noreply.github.com>
2026-06-01 20:36:25 +0200

    working on celery task

--- vin_decoder/tasks.py
+++ vin_decoder/tasks.py
@@ -13,3 +13,9 @@ from .utils import generate_car_raport_pdf, get_raport_data_from_redis
 logger = logging.getLogger(__name__)
 
 supabase : Client = create_client(settings.SUPABASE_URL, settings.SUPABASE_KEY)
+
+
+@shared_task
+def generate_pdf_task(vin, car_description):
+
+    pass
