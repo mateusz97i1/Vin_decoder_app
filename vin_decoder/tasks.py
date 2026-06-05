0bb622e7f3aecda3fd7c245618ce4f1ac5f85ef6
mateusz97i1 <120130259+mateusz97i1@users.noreply.github.com>
2026-06-05 10:36:15 +0200

    implemented new celery task

--- vin_decoder/tasks.py
+++ vin_decoder/tasks.py
@@ -18,35 +18,49 @@ supabase : Client = create_client(settings.SUPABASE_URL, settings.SUPABASE_KEY)
 @shared_task
 def generate_pdf_task(vin, car_description):
 
-    #get raport from cache
-    raw_info_data = get_raport_data_from_redis(car_description)
+    try:
 
-    if not raw_info_data:
-        logger.error(f"Cache miss for data: {car_description}")
-        return None
-    
-     #visual edit using markdown
-    car_info_data = markdown2.markdown(raw_info_data, extras= ['break-on-newline'])
+        #get raport from cache
+        raw_info_data = get_raport_data_from_redis(car_description)
 
-    #use html template to generate pdf file  
-    html_string = render_to_string('pdf_render/car_raport_pdf.html', context={
-        'raport_html': car_info_data,
-        'vin': vin
-    })
-    
-    pdf_file = generate_car_raport_pdf(html_content= html_string)
+        if not raw_info_data:
+            logger.error(f"Cache miss for data: {car_description}")
+            return None
+        
+        #visual edit using markdown
+        car_info_data = markdown2.markdown(raw_info_data, extras= ['break-on-newline'])
 
-    file_name = f"raport_VIN_{vin}.pdf"
-
-    #upload pdf to supabsae
-    response = supabase.storage.from_(settings.SUPABASE_BUCKET).upload(
-        file=pdf_file,
-        path=file_name,
-        file_options={
-        'upsert': 'true',
-        "content-type": "application/pdf"
+        #use html template to generate pdf file  
+        html_string = render_to_string('pdf_render/car_raport_pdf.html', context={
+            'raport_html': car_info_data,
+            'vin': vin
         })
+        
+        pdf_file = generate_car_raport_pdf(html_content= html_string)
 
+        #change pdf to bytes as supabase requiers
+        pdf_file.seek(0)
+        pdf_bytes = pdf_file.getvalue()
 
+        file_name = f"raport_VIN_{vin}.pdf"
 
-    pass
+        #upload pdf to supabsae
+        supabase.storage.from_(settings.SUPABASE_BUCKET).upload(
+            path=file_name,
+            file=pdf_bytes,
+            file_options={
+            'upsert': 'true',
+            "content-type": "application/pdf"
+            })
+
+        #retrieve URL donwload
+        download_url_public = supabase.storage.from_(settings.SUPABASE_BUCKET).get_public_url(
+            path = file_name,
+            options={"download": True})
+
+        return download_url_public
+
+    except Exception as e:
+
+        logger.exception(f"Error during uploading file to supabase or genereting public URL {e}")
+        return None
--- vinex/celery.py
+++ vinex/celery.py
@@ -8,3 +8,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE','vinex.settings')
 app = Celery('vinex')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+@app.task(bind=True, ignore_result=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
