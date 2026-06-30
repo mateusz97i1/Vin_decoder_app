83edcc66a91010032e6d0e24d1cf68bf45be0fcb
mateusz97i1 <120130259+mateusz97i1@users.noreply.github.com>
2026-06-30 19:20:29 +0200

    creating model for metadata raports

--- vin_decoder/models.py
+++ vin_decoder/models.py
@@ -1,3 +1,31 @@
+import uuid
+
 from django.db import models
 
+
 # Create your models here.
+class MetadataRaports(models.Model):
+
+    STATUS_CHOICES=[
+        ('PENDING', 'Pending'),
+        ('PROCESSING', 'Processing'),
+        ('COMPLETED', 'Completed'),
+        ('FAILED', 'Failed'),
+    ]
+
+    id= models.UUIDField(primary_key=True, default= uuid.uuid4, editable= False)
+
+    car_model=models.CharField(max_length=100, unique=True)
+    status= models.CharField(max_length=15, choices=STATUS_CHOICES)
+    supabase_url=models.URLField(max_length=500, blank=True, null=True)
+    created_at=models.DateTimeField(auto_now_add= True)
+    updated_at= models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+
+        verbose_name = "Metadata Report"
+        verbose_name_plural = "Metadata Reports"
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.car_model} ({self.status}) - {self.id} "
